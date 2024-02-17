cb4d663c5602ef96b156fe633f49299e07798c20
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-02-17 18:44:20 +0100

    Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
-import { nanoid } from "nanoid";
-import { Component } from "react";
-import styles from "./PhoneBook.module.css";
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import styles from './PhoneBook.module.css';
 import { ContactForm } from './ContactForm';
-import { ContactList } from "./ContactList";
-import { SearchFilter } from './SearchFilter'
+import { ContactList } from './ContactList';
+import { SearchFilter } from './SearchFilter';
 
 export class App extends Component {
   state = {
     contacts: [
-      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-      {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-      {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-  }
-  
-  handleChange = (event) => {
+  };
+
+  handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
@@ -27,34 +27,39 @@ export class App extends Component {
       name,
       number,
     };
-    if (this.state.contacts.find(({ name }) => name.toLocaleLowerCase() === newContact.name.toLocaleLowerCase())) {
-      alert(`${newContact.name} is already in contacts`)
-      return
+    if (
+      this.state.contacts.find(
+        ({ name }) =>
+          name.toLocaleLowerCase() === newContact.name.toLocaleLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
     }
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
-      filter:''
+      filter: '',
     }));
-  }
+  };
 
-  deleteContact = (deleteId) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter(({ id }) => id !== deleteId)
-    }))
-  }
+  deleteContact = deleteId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== deleteId),
+    }));
+  };
 
   render() {
     const { filter, contacts } = this.state;
-    const filteredContacts = contacts.filter(({ name }) => (
+    const filteredContacts = contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase())
-    ))
-    
+    );
+
     return (
       <div className={styles.container}>
         <h1 className={styles.hero}>Phonebook</h1>
-        <ContactForm addContact={this.addContact}/>
+        <ContactForm addContact={this.addContact} />
         <h2 className={styles.title}>Contacts</h2>
-        <SearchFilter value={filter} onChange={this.handleChange}/>
+        <SearchFilter value={filter} onChange={this.handleChange} />
         <ContactList
           contacts={filteredContacts}
           onDeleteContact={this.deleteContact}
